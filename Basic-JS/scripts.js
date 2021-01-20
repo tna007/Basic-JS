c0fb8500d90673840ff1cc28d7acba2211558440
tna007 <anh.to@edu.bc.fi>
2021-01-20 23:16:53 +0200

    Jackpot assigment

--- Basic-JS/scripts.js
+++ Basic-JS/scripts.js
@@ -1,25 +1,45 @@
-let chosenNumber = Math.floor(Math.random() *10)
-
-switch (chosenNumber) {
-    case 1:
-        console.log('One');
-        break;
-    case 2:
-        console.log('Two');
-        break;
-    case 3:
-        console.log('Three');
-        break;
-    case 4:
-        console.log('Four');
-        break;
-    case 5:
-        console.log('Five');
-        break;
-    case 6:
-        console.log('Six');
-        break;
-    default:
-        console.log('Dunno');
-        break;
+let a = Math.floor(Math.random() * 2);
+let b = Math.floor(Math.random() * 2);
+let c = Math.floor(Math.random() * 2);
+let d = Math.floor(Math.random() * 2);
+let e = Math.floor(Math.random() * 2);
+/* can't get 00000 to congrats phrase but went straight to default instead??? */
+/* switch ((a, b, c, d, e)) {
+  case 1:
+    if (a == b && b == c && c == d && d == e && a !== 1) {
+      console.log("Congrats!", a, b, c, d, e);
     }
+    break;
+
+  case 2:
+    if (a === b && b === c && c === d && d === e && a === 1) {
+      console.log(
+        "Congrats! You have won a jackpot prize",
+        a,
+        b,
+        c,
+        d,
+        e,
+        "the probability is ",
+        1 / 2 ** 5
+      );
+    }
+    break;
+
+  default:
+    console.log("You have not won a jackpot prize", a, b, c, d, e);
+    break;
+} */ if (
+  a == b &&
+  a == c &&
+  a == d &&
+  a == e
+) {
+  if (a == 0) {
+    console.log("Congrats!", a, b, c, d, e);
+  } else {
+    console.log("Jackpot!", a, b, c, d, e, "/n Your prob is:", 1 / 2 ** 5);
+  }
+} else {
+  console.log("Good luck next time!", a, b, c, d, e);
+}
