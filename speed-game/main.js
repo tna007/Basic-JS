b0fb19a2b39d174320356dad739a3c753c0d4f3c
tna007 <anh.to@edu.bc.fi>
2021-03-12 14:39:58 +0200

    Add setTimeOut logic to turn on/off lights

--- speed-game/main.js
+++ speed-game/main.js
@@ -1,11 +1,46 @@
-let circle = document.querySelectorAll("#light");
+let circles = document.querySelectorAll(".light");
+let over = document.querySelector(".result-over");
+let startBtn = document.getElementById("start");
+let stopBtn = document.getElementById("stop");
+let closeBtn = document.getElementById("close");
+let score = document.querySelectorAll("#result-score");
+let count = 0;
 
-for (let i = 0; i < circle.length; i++) {
-  circle[i].onclick = function () {
+for (let i = 0; i < circles.length; i++) {
+  circles[i].onclick = () => {
     clicked(i);
+  };
+  const clicked = (i) => {
+    console.log("clicked", i);
+    count++;
+    score.innerText = `Your score is ${count}`;
   };
 }
 
-const clicked = (i) => {
-  console.log("clicked", i);
+let timer;
+const startGame = () => {
+  startBtn.disabled = true;
+  let active, nextActive;
+  timer = setTimeout(function pickNew() {
+    nextActive = Math.floor(Math.random() * 4);
+    timer = setTimeout(pickNew, 1000);
+    console.log(nextActive);
+
+    if (active != null) {
+      circles[active].classList.remove("active");
+    }
+    circles[nextActive].classList.add("active");
+    active = nextActive;
+  }, 1000);
+};
+startBtn.addEventListener("click", () => {
+  console.log("Game started");
+  startGame();
+});
+stopBtn.onclick = () => {
+  clearTimeout(timer);
+  over.style.visibility = "visible";
+};
+closeBtn.onclick = () => {
+  window.location.reload();
 };
